74c686a562b2ab972e4d068b2238797f0dcc3e9c
Alexis-Duru <alexis.duru.pro@gmail.com>
2021-06-03 16:11:13 +0200

    edition profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
@@ -16,6 +18,27 @@ class ProfilController extends AbstractController
     {
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil/edit", name="app_profil_edit")
+     */
+    public function editProfil(EntityManagerInterface $em, SecurityController $security, Request $request): Response
+    {
+        $user = $security->getUser();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Your profile has been updated successfully.');
+
+            return $this->redirectToRoute('app_profil');
+        }
+
+        return $this->render('profil/edit.html.twig', [
+            'profil' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
